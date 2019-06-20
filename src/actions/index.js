e0665c3256012cb08eeaefb2337be32b6e86d403
Jason Cook <the.root.user@gmail.com>
2019-06-19 21:25:47 -0400

    Redux homework init
    Co-authored-by: renegade27 <foemaster@live.com>

--- src/actions/index.js
+++ src/actions/index.js
@@ -9,10 +9,14 @@ export const DECREMENT = 'DECREMENT';
 // logic of updating the central Redux store. That
 // is left to the reducer(s).
 
-export const increment = () => {
-  // Fill in this function
+export const increment = (count) => {
+  console.log(`have I even fired?`, count)
+
+    return {type: 'INCREMENT', payload: count}
 };
 
-export const decrement = () => {
-  // Fill in this function
+export const decrement = (count) => {
+  console.log(`have I also fired?`)
+
+    return {type: 'DECREMENT', payload: count}
 };
